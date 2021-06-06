36a278dfe38a6c1b3d2ecc5a8a08e822a4aa1961
lucasdinocloud <lucas.montegu@dinocloudconsulting.com>
2021-06-06 01:44:07 -0300

    Feat(home page): Create home page v1

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,14 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import { Chakra } from "../src/components/Chakra";
+import type { AppProps } from "next/app";
+import '../styles/globals.css';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <Chakra cookies={pageProps.cookies}>
+      <Component {...pageProps} />
+    </Chakra>
+  );
 }
-export default MyApp
+
+// re-export the reusable `getServerSideProps` function
+export { getServerSideProps } from "../src/components/Chakra";
---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,33 @@
+import { Flex, Stack, Text } from '@chakra-ui/react';
+import React from 'react'
+import Link from 'next/link'
+import { FiGithub, FiLinkedin } from "react-icons/fi";
+
+interface Props {
+  
+}
+
+const Footer = (props: Props) => {
+  return (
+    <Flex
+      as="footer"
+      align="center"
+      justify="space-between"
+      wrap="wrap"
+      w="100%"
+      p={8}
+    >
+      <Stack
+        direction="row"
+        spacing={4}
+        align="center"
+      >
+        <Link href="https://github.com/lucasmontegu"><a target="_blank" rel="noreferrer"><FiGithub size="22" /></a></Link>
+        <Link href="https://www.linkedin.com/in/lucasmontegu/"><a target="_blank" rel="noreferrer"><FiLinkedin size="22" /></a></Link>
+      </Stack>
+      <Stack><Text size="18">Coded with ❤️ by Lucas Montegú</Text></Stack>
+    </Flex>
+  )
+}
+
+export default Footer;
